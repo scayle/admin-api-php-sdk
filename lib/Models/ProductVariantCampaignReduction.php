48d2f59d37cb7ccd6c6b4e3ffe644ebc39149036
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2020-11-03 14:03:12 +0000

    Merge branch 'get-reduction' into 'master'
    get reductions
    
    See merge request aboutyou/cc-babo/admin-api!35

--- lib/Models/ProductVariantCampaignReduction.php
+++ lib/Models/ProductVariantCampaignReduction.php
@@ -3,6 +3,7 @@
 namespace AboutYou\Cloud\AdminApi\Models;
 
 /**
+ * @property int $id Backbone Core's internal reduction identifier.
  * @property int $productVariantId Backbone Core's internal product variant identifier.
  * @property string $productVariantReferenceKey Tenant provided product variant identifier.
  * @property int $reduction The reduction percentage applied to this specific variant.
