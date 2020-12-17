47764b58a07100280e091cdaa59a029ff6503f8c
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2020-12-17 19:05:10 +0000

    Merge branch 'change-cursor-to-string' into 'master'
    change cursor to string
    
    See merge request aboutyou/cc-babo/admin-api!62

--- lib/Models/ApiCollection.php
+++ lib/Models/ApiCollection.php
@@ -2,6 +2,20 @@
 
 namespace AboutYou\Cloud\AdminApi\Models;
 
+/**
+ * @property \AboutYou\Cloud\AdminApi\Models\Cursor $cursor
+ */
 class ApiCollection extends ApiObject
 {
+    protected $classMap = [
+        'cursor' => \AboutYou\Cloud\AdminApi\Models\Cursor::class,
+    ];
+
+    /**
+     * @return null|\AboutYou\Cloud\AdminApi\Models\Cursor
+     */
+    public function getCursor()
+    {
+        return $this->cursor;
+    }
 }
---
+++ lib/Models/Cursor.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AboutYou\Cloud\AdminApi\Models;
+
+/**
+ * @property string $next The cursor pointing to the next page. If null there is no next page available.
+ */
+class Cursor extends ApiObject
+{
+    protected $defaultValues = [
+    ];
+
+    protected $classMap = [
+    ];
+
+    protected $collectionClassMap = [
+    ];
+
+    protected $polymorphic = [
+    ];
+
+    protected $polymorphicCollections = [
+    ];
+}
