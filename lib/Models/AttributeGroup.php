f10b2472a8c582270c11cc1bfc5e044313aa73a5
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2021-07-27 14:33:36 +0000

    Merge branch 'BCO-4292' into 'master'
    [BCO-4292] Remove advancedList usage
    
    Closes BCO-4292
    
    See merge request aboutyou/cc-babo/admin-api!118

--- lib/Models/AttributeGroup.php
+++ lib/Models/AttributeGroup.php
@@ -8,15 +8,15 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property array $frontendName The localized attribute group name. At least the base language that is configured in Backbone Core is mandatory.
  * @property string $type An attribute group type.
  * @property bool $isShared Defines whether an attribute value is shared beetwen the entities or every entity has its own value.
- * It is not possible to share attributes of advanced and advancedList types, therefore isShared must be always false in this case.
+ * It is not possible to share attributes of advanced type, therefore isShared must be always false in this case.
  * @property string $level An attribute group level, which defines an entity the group is valid for.
  * @property array $structure A structure, which describes advanced attribute values.
- * Mandatory for advanced and advancedList types.
+ * Mandatory for the advanced type.
  * @property string[] $shopKeys A list of shop keys the attribute group is valid for.
  * The attribute group is valid for all shops if the field is omitted.
  * @property bool $isOverridablePerShop Defines if shop-specific attribute values are allowed for the given attribute group.
  * @property bool $isDifferentiating If an attribute group is differentiating, then every entity must have a unique attribute value.
- * This configuration is not applicable to advanced and advancedList types, neither to attribute groups marked as isShared=false.
+ * This configuration is not applicable to the advanced type, neither to attribute groups marked as isShared=false.
  */
 class AttributeGroup extends ApiObject
 {
