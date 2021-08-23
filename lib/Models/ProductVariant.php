2f7081205b162e450ef6fa23c59ff26293b9547a
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2021-08-23 13:06:06 +0000

    Merge branch 'task/BCO-4304' into 'master'
    BCO-4304 admin api specs
    
    Closes BCO-4304
    
    See merge request aboutyou/cc-babo/admin-api!136

--- lib/Models/ProductVariant.php
+++ lib/Models/ProductVariant.php
@@ -5,6 +5,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id ID assigned by BACKBONE Core.
  * @property string $referenceKey A key that uniquely identifies the variant of a product (usually an SKU) within the tenant's ecosystem.
+ * @property string $ean An ean that refers to a product variant .
  * @property Attribute[] $attributes A list of attributes attached to the product variant.
  * @property ProductVariantPrice[] $prices A list of prices attached to the product variant.
  */
