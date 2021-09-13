0cc3f50a8a30e327e55aadee308912ac8685e711
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2021-09-13 10:13:27 +0000

    Merge branch 'sub-task/BCO-4467-explain-warehouse-priority-logic-in-admin-api-dev-guide-and-specs' into 'master'
    [BCO-4467] Explain warehouse priority logic
    
    Closes BCO-4467
    
    See merge request aboutyou/cc-babo/admin-api!157

--- lib/Models/ShopWarehouse.php
+++ lib/Models/ShopWarehouse.php
@@ -5,7 +5,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id The ID of the warehouse created by Backbone Core.
  * @property string $referenceKey A key that uniquely identifies the warehouse within the tenant's ecosystem.
- * @property int $priority The priority of the warehouse.
+ * @property int $priority The priority of the warehouse. The higher the value, the higher the priority.
  * @property PackageGroup $packageGroup The assigned package group of the warehouse.
  */
 class ShopWarehouse extends ApiObject
