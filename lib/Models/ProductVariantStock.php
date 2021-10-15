f3f8ccd2ebd9f63cf97a21e21421606e29cc865d
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2021-10-15 15:53:21 +0000

    Merge branch 'bugfix/BCO-4293' into 'master'
    bugfix/BCO-4293 - update date formats specs
    
    See merge request aboutyou/cc-babo/admin-api!174

--- lib/Models/ProductVariantStock.php
+++ lib/Models/ProductVariantStock.php
@@ -5,7 +5,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $quantity Current quantity of SKU.
  * @property string $warehouseReferenceKey Reference key of warehouse for which the stock update is related to.
- * @property string $changedAt Date time when the stock changed in Iso8601 Zulu format.
+ * @property string $changedAt Date time when the stock changed in Iso8601 format.
  */
 class ProductVariantStock extends ApiObject
 {
