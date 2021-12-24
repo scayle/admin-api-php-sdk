ae7555510e6c9ceda2cf3c8df6c3c0a09ae8869f
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2021-12-24 09:24:16 +0000

    Merge branch 'story/BCO-4776-createvoucher-request-support-more-criteria' into 'master'
    Resolve BCO-4776 "Story/ createvoucher request support more criteria"
    
    Closes BCO-4776
    
    See merge request aboutyou/cc-babo/admin-api!198

--- lib/Models/VoucherCriterion.php
+++ lib/Models/VoucherCriterion.php
@@ -5,7 +5,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id
  * @property string $key
- * @property string $type
+ * @property string $type In case shopCategoryId key used that should be corresponded for country type of application instead of shop.
  * @property mixed[] $value
  */
 class VoucherCriterion extends ApiObject
