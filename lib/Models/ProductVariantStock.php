e8ed435427a62754e2bfc9493f61f0255cee4ab6
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2022-01-24 11:52:05 +0000

    Merge branch 'BCO-4801' into 'master'
    [BCO-4801] Add sellableWithoutStock flag
    
    Closes BCO-4801
    
    See merge request aboutyou/cc-babo/admin-api!201

--- lib/Models/ProductVariantStock.php
+++ lib/Models/ProductVariantStock.php
@@ -6,6 +6,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property int $quantity Current quantity of SKU.
  * @property string $warehouseReferenceKey Reference key of warehouse for which the stock update is related to.
  * @property string $changedAt Date time when the stock changed in Iso8601 format.
+ * @property bool $sellableWithoutStock Defines if the variant can be sold even when the available stock is 0.
  */
 class ProductVariantStock extends ApiObject
 {
