5683697f2f3e09e4fa6ab9a561698b107ce921c2
admin-api-gitlab-runner <team-operations+noreply@aboutyou.com>
2022-03-18 14:15:37 +0000

    Merge branch 'task/BCO-4869-1' into 'master'
    BCO-4869
    
    Closes BCO-4869
    
    See merge request aboutyou/cc-babo/admin-api!224

--- lib/Models/ShopCountry.php
+++ lib/Models/ShopCountry.php
@@ -10,8 +10,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property string[] $supportedLanguageCodes List of supported language codes.
  * @property string $url Url of the shop country.
  * @property bool $active Defines if the shop country is active.
- * @property string $currencyCode The currency used in the shop country.
- * @property bool $deleted Defines it the shop country is deleted.
+ * @property bool $deleted Defines if the shop country is deleted.
  * @property string $priceGroupKey Key of the price group the shop country is assigned to.
  * @property Assortment $assortment
  * @property ShopCountryWarehouse[] $warehouses A list of warehouses attached to the shop country.
