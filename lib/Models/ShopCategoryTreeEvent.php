4122c4e4991c3e489ad3274c50b333c5130bcd00
Omar Fawzy <omar.fawzy@aboutyou.com>
2022-05-19 15:31:04 +0000

    Merge branch 'task/BCO-5085' into 'master'
    BCO-5085 add specs
    
    Closes BCO-5085
    
    See merge request aboutyou/cc-babo/admin-api!254

---
+++ lib/Models/ShopCategoryTreeEvent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AboutYou\Cloud\AdminApi\Models;
+
+class ShopCategoryTreeEvent extends ApiObject
+{
+    protected $defaultValues = [
+    ];
+
+    protected $classMap = [
+    ];
+
+    protected $collectionClassMap = [
+    ];
+
+    protected $polymorphic = [
+    ];
+
+    protected $polymorphicCollections = [
+    ];
+}
