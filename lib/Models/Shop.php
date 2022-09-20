4de661e2d089084797240a1caf75a8d495c21995
Eduardo Garcia <eduardo.garcia@aboutyou.com>
2022-09-20 14:41:54 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-5391' into 'master'
    Fix typos in API reference
    
    Closes BCO-5391
    
    See merge request aboutyou/cc-babo/admin-api!277

--- lib/Models/Shop.php
+++ lib/Models/Shop.php
@@ -7,13 +7,13 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property string $key A key that uniquely identifies the shop within the tenant's ecosystem. Must be **exactly two characters** long.
  * @property string $name Full name of the shop.
  * @property string $logoUrl The URL of the logo assigned to the shop.
- * @property AssetSource $logoSource A source specifieng where to download the logo from.
+ * @property AssetSource $logoSource A source specifying where to download the logo from.
  * @property bool $active Defines if the shop is active.
- * @property bool $deleted Defines it the shop is deleted.
+ * @property bool $deleted Defines if the shop is deleted.
  * @property string $priceGroupKey Key of the price group the shop is assigned to.
- * @property int $shopCategoryTreeId Defines shop categories tree id. Can be set only on creation.
+ * @property int $shopCategoryTreeId Defines shop category tree id. Can be set only on creation.
  * @property int $companyId The ID of the company the shop is assigned to.
- * @property ShopCountry[] $countries The countries assigned to the shop.
+ * @property ShopCountry[] $countries The countries assigned to the shop. Can be set only on creation.
  * @property mixed $customData Arbitrary fields assigned to Shop
  */
 class Shop extends ApiObject
