2494ce7592da5b52b13e1c93ac0047d154fd79c6
Ivan Babenko <ivan.babenko@aboutyou.com>
2023-03-01 13:02:30 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-5855' into 'master'
    [BCO-5855] Add shopCountrySpecific compatibility note
    
    Closes BCO-5855
    
    See merge request aboutyou/cc-babo/admin-api!306

--- lib/Models/Attribute.php
+++ lib/Models/Attribute.php
@@ -7,7 +7,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property string $type The attribute type.
  * @property mixed $value The attribute value where the datatype is defined by the type property.
  * @property bool $isLocked Specifies if the attribute was locked via SCAYLE Panel.
- * @property AttributeShopCountrySpecific[] $shopCountrySpecific Used to override the attribute value for a specific shop country.
+ * @property AttributeShopCountrySpecific[] $shopCountrySpecific Used to override the attribute value for a specific shop country. Only supported by products, product masters and variants.
  */
 class Attribute extends ApiObject
 {
