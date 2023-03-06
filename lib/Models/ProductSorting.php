0e5384a140e5dc76aa8c75687057fab3b116698f
Ivan Babenko <ivan.babenko@aboutyou.com>
2023-03-06 14:10:29 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-5876' into 'master'
    [BCO-5876] Add countryCode in product sorting schema
    
    Closes BCO-5876
    
    See merge request aboutyou/cc-babo/admin-api!305

--- lib/Models/ProductSorting.php
+++ lib/Models/ProductSorting.php
@@ -6,6 +6,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property int $productId Backbone Core's internal product identifier.
  * @property string $productReferenceKey Tenant provided product identifier.
  * @property string $shopKey A key that uniquely identifies the shop within the tenant's ecosystem.
+ * @property string $countryCode ISO 3166 alpha 2 country code.
  * @property string $sortKey The sort key.
  * @property int $sortValue The value used for sorting.
  */
