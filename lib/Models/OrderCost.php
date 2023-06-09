98eee7a3206ff8b4a3ad9593d6b3c1cafbc75f25
Ivan Babenko <ivan.babenko@aboutyou.com>
2023-06-09 12:02:36 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6110' into 'master'
    [BCO-6110] Add additional order.cost fields
    
    Closes BCO-6110
    
    See merge request aboutyou/cc-babo/admin-api!325

--- lib/Models/OrderCost.php
+++ lib/Models/OrderCost.php
@@ -6,8 +6,11 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property OrderFee[] $appliedFees
  * @property OrderReduction[] $appliedReductions
  * @property OrderCostTax $tax
- * @property int $withTax
- * @property int $withoutTax
+ * @property int $withTax The price is calculated including taxes and all applicable reductions such as discounts for sale and campaigns (should a campaign key be provdided on the request).
+ * @property int $withoutTax This price excludes taxes, but also includes all applicable reductions.
+ * @property int $withTaxWithMembershipDiscountWithoutServiceCosts The price is calculated including taxes and all applicable reductions such as discounts for sale and campaigns (should a campaign key be provdided on the request), and membership discount.
+ * @property int $withoutTaxWithMembershipDiscount This price excludes taxes, but also includes all applicable reductions and membership discount.
+ * @property int $costCapture The exact cost captured: order total value - giftcard value - total membership discount
  */
 class OrderCost extends ApiObject
 {
