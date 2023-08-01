7a304f922eab4503ce9451510db0e63e53669616
Ivan Babenko <ivan.babenko@aboutyou.com>
2023-08-01 11:45:15 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6155' into 'master'
    [BCO-6155] Update assortment schema
    
    Closes BCO-6155
    
    See merge request aboutyou/cc-babo/admin-api!329

--- lib/Models/Assortment.php
+++ lib/Models/Assortment.php
@@ -6,6 +6,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property MasterCategoryAssortmentConfiguration $masterCategories Configuration of master category includes/excludes.
  * @property ProductAssortmentConfiguration $products Configuration of product includes/excludes.
  * @property AttributeAssortmentConfiguration[] $attributes Configuration of attribute includes/excludes.
+ * @property MerchantAssortmentConfiguration $merchantReferenceKeys Configuration of merchant includes/excludes.
  */
 class Assortment extends ApiObject
 {
@@ -15,6 +16,7 @@ class Assortment extends ApiObject
     protected $classMap = [
         'masterCategories' => \AboutYou\Cloud\AdminApi\Models\MasterCategoryAssortmentConfiguration::class,
         'products' => \AboutYou\Cloud\AdminApi\Models\ProductAssortmentConfiguration::class,
+        'merchantReferenceKeys' => \AboutYou\Cloud\AdminApi\Models\MerchantAssortmentConfiguration::class,
     ];
 
     protected $collectionClassMap = [
---
+++ lib/Models/MerchantAssortmentConfiguration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AboutYou\Cloud\AdminApi\Models;
+
+/**
+ * @property string[] $include Merchant reference keys to include.
+ * @property string[] $exclude Merchant reference keys to exclude.
+ */
+class MerchantAssortmentConfiguration extends ApiObject
+{
+    protected $defaultValues = [
+    ];
+
+    protected $classMap = [
+    ];
+
+    protected $collectionClassMap = [
+    ];
+
+    protected $polymorphic = [
+    ];
+
+    protected $polymorphicCollections = [
+    ];
+}
