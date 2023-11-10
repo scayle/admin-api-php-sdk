f0109d3d86df3422691638c758b8b88ad23fa066
George Bajura <george.bajura@scayle.com>
2023-11-10 14:54:36 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6381' into 'master'
    Resolve BCO-6381
    
    Closes BCO-6381
    
    See merge request aboutyou/cc-babo/admin-api!361

--- lib/Models/AttributeGroup.php
+++ lib/Models/AttributeGroup.php
@@ -5,11 +5,11 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id The ID of the attribute group created by SCAYLE.
  * @property string $name A name that uniquely identifies an attribute group.
- * @property array $frontendName The localized attribute group name. At least the base language that is configured in SCAYLE is mandatory.
+ * @property array $frontendName The localized attribute group name. It must include at least the default language that is configured in SCAYLE.
  * @property string $type An attribute group type.
- * @property bool $isShared Defines whether an attribute value is shared beetwen the entities or every entity has its own value.
- * It is not possible to share attributes of advanced type, therefore isShared must be always false in this case.
- * @property string $level An attribute group level, which defines an entity the group is valid for.
+ * @property bool $isShared Specifies whether an attribute value is shared among multiple entities or if each entity has its own unique value.
+ * It's important to note that attributes of advanced types cannot be shared, so 'isShared' must always be set to false in such cases.
+ * @property string $level Defines the attribute group level, specifying the type of entity it is applicable to.
  * @property array $structure A structure, which describes advanced attribute values.
  * Mandatory for the advanced type.
  * @property AttributeGroupShopCountry[] $shopCountries A list of shop countries the attribute group is valid for.
--- lib/Models/VoucherCriterion.php
+++ lib/Models/VoucherCriterion.php
@@ -5,7 +5,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id
  * @property string $key
- * @property string $type In case shopCategoryId key used that should be corresponded for country type of application instead of shop.
+ * @property string $type If the 'shopCategoryId' key is utilized, it should be associated with a 'country' type of application rather than 'shop'.
  * @property mixed[] $value
  */
 class VoucherCriterion extends ApiObject
