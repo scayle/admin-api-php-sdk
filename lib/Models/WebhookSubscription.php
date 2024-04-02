7334b1595ccc5ceda14ab1557fa51d05c03361d9
Plinio Ribeiro <plinio.ribeiro@scayle.com>
2024-04-02 14:20:54 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6676' into 'master'
    BCO-6676 redact credentials
    
    Closes BCO-6676
    
    See merge request aboutyou/cc-babo/admin-api!382

--- lib/Models/WebhookSubscription.php
+++ lib/Models/WebhookSubscription.php
@@ -6,7 +6,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property int $id The unique webhook identifier.
  * @property string $event Name of the hook event.
  * @property int $version The version of the hook.
- * @property string $url The URL that will be called whenever the hook is triggered.
+ * @property string $url The URL that will be called whenever the hook is triggered. Sensitive data will be obfuscated in responses.
  */
 class WebhookSubscription extends ApiObject
 {
