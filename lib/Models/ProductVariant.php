e5d2905515fc19506460d19d6df1dc409bd67c9f
Zelimkhan Khalidov <zelimkhan.khalidov@aboutyou.com>
2024-04-24 15:26:26 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6750' into 'master'
    BCO-6750 extend variant by merchantReferenceKeys
    
    Closes BCO-6750
    
    See merge request aboutyou/cc-babo/admin-api!392

--- lib/Models/ProductVariant.php
+++ lib/Models/ProductVariant.php
@@ -5,6 +5,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id ID assigned by SCAYLE.
  * @property string $referenceKey A key that uniquely identifies the variant of a product (usually an SKU) within the tenant's ecosystem.
+ * @property string[] $merchantReferenceKeys A list of merchant reference keys the variant belongs to.
  * @property string $ean An ean that refers to a product variant .
  * @property Attribute[] $attributes A list of attributes attached to the product variant.
  * @property ProductVariantPrice[] $prices A list of prices attached to the product variant.
