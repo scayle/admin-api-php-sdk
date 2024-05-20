31368f6da79a558b787d04c8b90abc3dd09d86a6
George Bajura <george.bajura@scayle.com>
2024-05-20 07:17:04 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6815' into 'master'
    [BCO-6815] extend ShopCategoryCountry, add shopCountryId property
    
    Closes BCO-6815
    
    See merge request aboutyou/cc-babo/admin-api!408

--- lib/Models/ShopCategoryCountry.php
+++ lib/Models/ShopCategoryCountry.php
@@ -4,6 +4,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
 
 /**
  * @property string $countryCode ISO 3166 alpha 2 country code.
+ * @property int $shopCountryId Id of Shop Country.
  * @property string $path String representation of the URL path to the category.
  * @property bool $isActive Declares whether the shop category is active or not.
  * @property bool $isVisible Declares whether the shop category is visible in the shop or not.
