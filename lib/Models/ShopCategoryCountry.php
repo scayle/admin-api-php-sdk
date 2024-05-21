47997d9562229828dd03e192cd9b1ad4fce2dbff
Sagar Karmur <sagar.karmur@aboutyou.com>
2024-05-21 08:42:39 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-6829' into 'master'
    Resolve BCO-6829
    
    Closes BCO-6829
    
    See merge request aboutyou/cc-babo/admin-api!404

--- lib/Models/ShopCategoryCountry.php
+++ lib/Models/ShopCategoryCountry.php
@@ -8,6 +8,7 @@ namespace AboutYou\Cloud\AdminApi\Models;
  * @property string $path String representation of the URL path to the category.
  * @property bool $isActive Declares whether the shop category is active or not.
  * @property bool $isVisible Declares whether the shop category is visible in the shop or not.
+ * @property bool $isExcludedFromSearch Declares whether the shop category country should be excluded from search.
  * @property ShopCategoryProperty[] $properties The properties assigned to the shop category.
  * @property mixed $customData
  */
