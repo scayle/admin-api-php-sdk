b09588849873926df165303fd3670673a3833497
Sagar Karmur <sagar.karmur@aboutyou.com>
2024-09-02 14:48:15 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-7012' into 'master'
    Resolve BCO-7012
    
    Closes BCO-7012
    
    See merge request aboutyou/cc-babo/admin-api!429

---
+++ lib/Models/Transaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AboutYou\Cloud\AdminApi\Models;
+
+/**
+ * @property string $transactionId
+ * @property string $operationStatus
+ */
+class Transaction extends ApiObject
+{
+    protected $defaultValues = [
+    ];
+
+    protected $classMap = [
+    ];
+
+    protected $collectionClassMap = [
+    ];
+
+    protected $polymorphic = [
+    ];
+
+    protected $polymorphicCollections = [
+    ];
+}
