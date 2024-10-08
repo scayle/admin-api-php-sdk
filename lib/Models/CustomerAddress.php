041ba5e14c4ac42f3ffde06d766724764c422eaf
Eduardo Garcia <eduardo.garcia@scayle.com>
2024-10-08 11:51:43 +0000

    [AdminAPI-dev-master] Merge branch 'BCO-7181-validate-admin-api-functionality-and-documentation' into 'master'
    Resolve BCO-7181 "Validate admin api functionality and documentation"
    
    Closes BCO-7181
    
    See merge request aboutyou/cc-babo/admin-api!443

--- lib/Models/CustomerAddress.php
+++ lib/Models/CustomerAddress.php
@@ -5,10 +5,10 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $id
  * @property string $referenceKey External reference set by the client to integrate third party systems.
- * @property string $street Street is the mandatory string value in recepient's address
- * @property string $houseNumber House number of the recepient address
+ * @property string $street Street is the mandatory string value in recipient's address
+ * @property string $houseNumber House number of the recipient address
  * @property string $additional Additional data pertaining to the address
- * @property string $zipCode Zip code is a postal code of recepients location. Its a madatory value
+ * @property string $zipCode Zip code is the postal code of the recipient's location. Its a mandatory value
  * @property string $city City of the recipient. It is a mandatory value
  * @property string $countryCode ISO 3166-1 alpha-3 country code
  * @property CustomerAddressCollectionPoint $collectionPoint Details of the point where the parcel is received (if used)
