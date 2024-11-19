34b3f8c5fbac9cd15ad95c56411cd276db1bae33
Matthias Reichenbecher <matthias.reichenbecher@scayle.com>
2024-11-19 10:02:32 +0000

    [AdminAPI-dev-master] Merge branch 'task/BCO-7298' into 'master'
    BCO-7298 added a note that includeProductIds and excludeProductIds are limited...
    
    Closes BCO-7298
    
    See merge request aboutyou/cc-babo/admin-api!452

--- lib/Models/ShopCategoryProductSet.php
+++ lib/Models/ShopCategoryProductSet.php
@@ -5,8 +5,8 @@ namespace AboutYou\Cloud\AdminApi\Models;
 /**
  * @property int $productSetId The ID of the product set.
  * @property int $referencedProductSetId The ID of the referenced product set.
- * @property int[] $includeProductIds Product IDs, which are explicitly included in the shop category.
- * @property int[] $excludeProductIds Product IDs, which are explicitly excluded from the shop category.
+ * @property int[] $includeProductIds Product IDs, which are explicitly included in the shop category (limited to a maximum of 10,000 Product IDS).
+ * @property int[] $excludeProductIds Product IDs, which are explicitly excluded from the shop category (limited to a maximum of 10,000 Product IDS).
  * @property bool $isNew Declares whether the product set should only contain new products.
  * @property ShopCategoryAttributeCriterion[] $attributes Defines which products to include or exclude by attributes.
  */
