f40ea5ac001ccd4bac00768ebe241a180b9ae256
Zelimkhan Khalidov <zelimkhan.khalidov@aboutyou.com>
2026-04-21 12:08:39 +0000

    [AdminAPI-dev-master] bbbbb2dd

--- lib/AbstractApi.php
+++ lib/AbstractApi.php
@@ -77,7 +77,7 @@ abstract class AbstractApi
     private function makeHeaders(array $headers, bool $withContentType)
     {
         $headers[self::AUTH_HEADER_NAME] = $this->getAccessToken();
-        $headers[self::SDK_VERSION] = 'dev-master-41cae56a';
+        $headers[self::SDK_VERSION] = 'dev-master-bbbbb2dd';
         $headers['Accept'] = 'application/json, */*';
 
         if ($withContentType) {
