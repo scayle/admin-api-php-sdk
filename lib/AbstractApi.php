1b903de2ec02a561594d5d9e6e989700944af3ea
project-access-token <project_67858911_bot_ec71b607cffdce022aad886d3e0590df@noreply.gitlab.com>
2026-06-09 08:00:44 +0000

    [AdminAPI-dev-master] d31d1968

--- lib/AbstractApi.php
+++ lib/AbstractApi.php
@@ -77,7 +77,7 @@ abstract class AbstractApi
     private function makeHeaders(array $headers, bool $withContentType)
     {
         $headers[self::AUTH_HEADER_NAME] = $this->getAccessToken();
-        $headers[self::SDK_VERSION] = 'dev-master-6b903f4d';
+        $headers[self::SDK_VERSION] = 'dev-master-d31d1968';
         $headers['Accept'] = 'application/json, */*';
 
         if ($withContentType) {
