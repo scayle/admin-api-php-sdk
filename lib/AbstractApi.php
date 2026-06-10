bbc62a7b1c473c57a8a91454ef3096cfcfeaa9f3
Matthias Reichenbecher <matthias.reichenbecher@scayle.com>
2026-06-10 08:31:33 +0000

    [AdminAPI-v2.25.0] Release

--- lib/AbstractApi.php
+++ lib/AbstractApi.php
@@ -77,7 +77,7 @@ abstract class AbstractApi
     private function makeHeaders(array $headers, bool $withContentType)
     {
         $headers[self::AUTH_HEADER_NAME] = $this->getAccessToken();
-        $headers[self::SDK_VERSION] = 'dev-master-d31d1968';
+        $headers[self::SDK_VERSION] = 'v2.25.0';
         $headers['Accept'] = 'application/json, */*';
 
         if ($withContentType) {
