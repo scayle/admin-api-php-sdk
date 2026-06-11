67b612f216ee6c7e61f34bea7a8dd10c16e2337e
project-access-token <project_67858911_bot_ec71b607cffdce022aad886d3e0590df@noreply.gitlab.com>
2026-06-11 13:09:05 +0000

    [AdminAPI-dev-master] cf09ccaf

--- lib/AbstractApi.php
+++ lib/AbstractApi.php
@@ -77,7 +77,7 @@ abstract class AbstractApi
     private function makeHeaders(array $headers, bool $withContentType)
     {
         $headers[self::AUTH_HEADER_NAME] = $this->getAccessToken();
-        $headers[self::SDK_VERSION] = 'dev-master-f7137c03';
+        $headers[self::SDK_VERSION] = 'dev-master-cf09ccaf';
         $headers['Accept'] = 'application/json, */*';
 
         if ($withContentType) {
