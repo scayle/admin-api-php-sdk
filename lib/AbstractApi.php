701d73a1ba63a6a20e8aa9452a8c808f78489862
Vladimir Chmykov <vladimir.chmykov@scayle.com>
2026-06-24 09:30:11 +0000

    [AdminAPI-v2.26.0] Release

--- lib/AbstractApi.php
+++ lib/AbstractApi.php
@@ -77,7 +77,7 @@ abstract class AbstractApi
     private function makeHeaders(array $headers, bool $withContentType)
     {
         $headers[self::AUTH_HEADER_NAME] = $this->getAccessToken();
-        $headers[self::SDK_VERSION] = 'dev-master-cf09ccaf';
+        $headers[self::SDK_VERSION] = 'v2.26.0';
         $headers['Accept'] = 'application/json, */*';
 
         if ($withContentType) {
